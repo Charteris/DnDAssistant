0c7ef2e62ecac0626f3d368d5e0a517573a80dbe
Lachlan <charterislachlan@gmail.com>
2024-04-04 19:16:28 +1000

    Updates

---
+++ precache-manifest.a828dfe347dcf53dce0e3df83a59b413.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c1c9465734963f8a3bc263ca69275f",
+    "url": "/DnDAssistant/index.html"
+  },
+  {
+    "revision": "c6e9e67ef6345aa6df03",
+    "url": "/DnDAssistant/static/js/2.b77e118f.chunk.js"
+  },
+  {
+    "revision": "36714b759cb4486a8fd5",
+    "url": "/DnDAssistant/static/js/main.2c00b5a3.chunk.js"
+  },
+  {
+    "revision": "e61a8844c13146406efd",
+    "url": "/DnDAssistant/static/js/runtime~main.c9fe5edb.js"
+  },
+  {
+    "revision": "10b752dadc94d8a70ed40f46973cb101",
+    "url": "/DnDAssistant/static/media/Avandria.10b752da.png"
+  }
+]);
